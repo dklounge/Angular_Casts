b00e4a96ce52262d89af74bf9b57e27cb7eb2077
David Kim <daviddarden11@gmail.com>
2013-11-29 21:18:47 -0800

    api implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 AngularCasts::Application.routes.draw do
 
-  resources :screencasts
+  scope :api do
+    get "/screencasts(.:format)" => "screencasts#index"
+    get "/screencasts/:id(.:format)" => "screencasts#show"
+  end
 
 end
